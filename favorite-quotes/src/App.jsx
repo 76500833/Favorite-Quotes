41acab3c092c3e3311c488b9ab8cb5864be12c39
Ethan Metz <76500833@protonmail.com>
2024-09-16 21:14:06 -0800

    init

---
+++ favorite-quotes/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Word from './components/Word'
+import './App.css'
+
+function App() {
+
+
+  return (
+    < Word />
+  )
+}
+
+export default App
---
+++ favorite-quotes/src/components/Word.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+
+function Word() {
+    return (
+        <>
+            <h1>Quotes on the Word</h1>
+            <div className="quotes">
+                <ol>
+                    <li><p>In His words you find the purest integrity.
+                        They are Him and He is them. It is to His Word that we bow
+                        as we bow to His glory for His Word and His glory are the
+                        same. Nothing but the
+                        Word can push back darkness, for nothing but the Word has
+                        the power of God so purely and clearly. </p>
+                    </li>
+                    <li>
+                        <p>
+                            The battle is to accept love. For God is love and God is the word and to
+                            love the word is to love God. And to be loved by the word is to be loved by God.
+                            And the words of Jesus, who is the word made manifest are words of love made manifest.
+                            Trust every word, embrace every word. Define a new rationality by every word. For the Father
+                            knows the brokenness of your heart and hungers to heal it through is son and through the
+                            body of his son, who are the body of Christ. There is only healing in the revelation of the
+                            word of the Fathers love. And this revelation can come only through the Holy Spirit.
+                            And the Holy Spirit will come only on recognition of what the son has done.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Every Word that
+                            proceeds from your mouth is either confirmation or
+                            contestation.
+                            Sweet and bitter water cannot flow from the same spring.
+                            Neither can confirmation and contestation flow from the
+                            same lips. With each contestation is every preceding
+                            confirmation poisoned and thus must the old wineskin be
+                            replaced with a new one. All that must be spoken must be
+                            in confirmation of the Word. All that must be spoken must
+                            say Yay and Amen.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            It is in unity with the Word that power, love and the sound
+                            mind are made manifest. For if the Word is considered Holy
+                            and revered above circumstance and feeling, then shall the
+                            power of the Word produce the desired fruit. It is in
+                            reverence of the Word and confirmation of the Word that
+                            unconditional love is made possible. With the wisdom of
+                            man, most things are impossible but through the Word
+                            manifest all things are possible. For to be with the Word is
+                            to be with God. That is to say, to be aligned with the Word
+                            is to be aligned with God.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Do as the son commands and the promises of the son will be with you till the end of the age.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Might and power are enemies of the cross. Move by the Spirit, pursue the things of the Spirit. Desire spiritual gifts, test your tree by the fruit of the Spirit.
+                            Worldly wisdom will lead you to a precipice of compromise. Even the words of the Son as letter and law will bring death, rejection and shame but by the Spirit they bring life.
+                            Might and power are enemies of the cross.
+
+                            The kingdom is all and all that is in the kingdom is of the Spirit and by the Spirit.
+                            Might and power are the tools of a different kingdom, a kingdom of lies and counterfeits.
+
+                            If the fruit of the Spirit is not present, you are at the mercy of might and power.
+                            Sheep and wheat move purely by the Spirit.
+                            Pursue not the wisdom man, it is foolishness and vanity. Pursue the Spirit and the fruit of the Spirit. That which is obedience to the Holy Spirit will always produce the fruit of the Spirit. The fruit of the spirit is to be your letter and law. Might and power produce pain and anguish but the Spirit brings life.
+
+                            The life of the Spirit produces the fruit of the Spirit.
+                            But the life of might and power produces war and anguish.
+                            Indeed, there is not one fruit of the Spirit that hurts or offends.
+                            But might and power bring with them condescension and rejection.
+
+                            Seek then only that which is spirit. What is this earthly realm but a manifestation of what occurs in the spirit?
+                            Seek first the spiritual kingdom and all that is required in the physical kingdom will be added to you. Every instruction from heaven is an instruction to fetch reward.
+                            Ignore none of them! Heaven on earth is when that person you choose to be is laden with the fruit of the spirit. Without love you are nothing. Your works are as garbage before the throne.
+
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            If you are stagnent for even one day, the enemy will use yesterdays wine against you,
+                            you need fresh wine every day. His mercies renew every morning because every morning there
+                            are new challenges to face.
+                        </p>
+                    </li>
+                </ol>
+            </div >
+        </>
+    )
+}
+export default Word;
